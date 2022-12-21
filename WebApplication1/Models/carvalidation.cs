c337d7ad1674516aa8380c0ac398a5922ebe1763
MD Atif Hossain <md.atif.hossain@g.bracu.ac.bd>
2022-12-21 23:35:14 +0600

    master

---
+++ WebApplication1/Models/carvalidation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+
+    [MetadataType(typeof(carregMetaData))]
+    public partial class carReg
+    {
+        public class carregMetaData 
+        { 
+        
+        [DisplayName("ID")]
+        public int ID { get; set; }
+
+        [DisplayName("Book Number")]
+        public string Car_Number { get; set; }
+
+        [DisplayName("Book Name")]
+        public string Model { get; set; }
+
+        [DisplayName("Price")]
+        public string Availability { get; set; }
+
+
+        }
+    }
+}
